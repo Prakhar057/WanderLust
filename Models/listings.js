9fa6c22468fcf7d4a7ceebdfe805ce5150fd52be
Prakhar Gupta <guptaprakhar057dev@gmail.com>
2025-10-26 23:43:34 +0530

    addind reviews

--- Models/listings.js
+++ Models/listings.js
@@ -1,35 +1,44 @@
 const mongoose = require("mongoose");
 
-const listingSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const listingSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    descriptions: {
+      type: String,
+    },
+    image: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1531183436556-51f742660c8d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2140",
+      set: (v) =>
+        v === ""
+          ? "https://images.unsplash.com/photo-1531183436556-51f742660c8d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2140"
+          : v,
+    },
+    price: {
+      type: Number,
+    },
+    location: {
+      type: String,
+    },
+    country: {
+      type: String,
+    },
+    reviews: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
   },
-  descriptions: {
-    type: String,
-  },
-  image: {
-    type: String,
-    default:
-      "https://images.unsplash.com/photo-1531183436556-51f742660c8d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2140",
-    set: (v) =>
-      v === ""
-        ? "https://images.unsplash.com/photo-1531183436556-51f742660c8d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2140"
-        : v,
-  },
-  price: {
-    type: Number,
-  },
-  location: {
-    type: String,
-  },
-  country: {
-    type: String,
-  },
-},{
-  strict : true,
-  strictQuery : true
-});
+  {
+    strict: true,
+    strictQuery: true,
+  }
+);
 
 const Listing = mongoose.model("Listing", listingSchema);
 
